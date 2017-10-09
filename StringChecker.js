4fdacd67d8664d07c74560f696dadac26a93778c
Livio <liviozanardo@gmail.com>
2017-10-09 18:15:31 -0400

    Hello from PSM

---
+++ StringChecker.js
@@ -0,0 +1,75 @@
+
+CURRENT ITERATION:
+
+var site = document.documentElement.innerHTML;
+var regex = "https://join-hits.teamskeet.com/";
+site.match(regex)
+
+
+function checkString(site);
+
+
+/////
+var url = "http://newgrounds.com/"
+
+
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "http://google.com/", false ); 
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+var xhr = new XMLHttpRequest();
+xhr.onload = function() {
+  console.log(this.responseXML.title);
+}
+
+xhr.open("GET", "file.html");
+xhr.responseType = "document";
+xhr.send();
+
+
+
+///Receiving an HTTP request
+var regex = "https://join-hits.teamskeet.com/"
+function checkAjax(){
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function("https://join.teensloveblackcocks.com/signup/signup.php?step=signup&nats=NDA2Ni42OC42NC41ODIuMC4wLjAuMC4w&switched=1&strack=0&tpl=join_mobile") {
+  
+console.log(this.responseXML.title);
+}
+
+
+xhr.open("GET", site);
+
+xhr.responseType = "document";
+
+xhr.send();
+
+console.log(xhr.responseXML.match(regex));
+
+//////email randomizer code from stackoverflow
+var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+var string = '';
+for(var ii=0; ii<15; ii++){
+    string += chars[Math.floor(Math.random() * chars.length)];
+}
+alert(string + '@domain.com');
+********
+
+//////auto-fill the form and hit submit button 
+document.getElementById('join').value = 'foo';
+document.getElementById('login_form').submit();
+********
+
+///loop through form elements and write "foo" in each field
+var formElements = document.getElementsByClassName('join_input');
+
+for (var i=0; i< formElements.length; i++){
+	formElements[i].value = "foo";
+}
+
+*****
+
