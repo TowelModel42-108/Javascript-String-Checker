000fdb90c0fd747bfa653de0a8c59a06c7481aef
Livio <liviozanardo@gmail.com>
2017-10-10 10:10:46 -0400

    Credentials fully generated.

--- StringChecker.js
+++ StringChecker.js
@@ -8,14 +8,15 @@ site.match(regex)
 
 function checkString(site);
 
-
 /////
+////
+/////THE WORKBENCH STARTS HERE
 var url = "http://newgrounds.com/"
 
 
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "http://google.com/", false ); 
+    xmlHttp.open( "GET", "http://google.com/", false );
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
@@ -28,8 +29,10 @@ xhr.onload = function() {
 xhr.open("GET", "file.html");
 xhr.responseType = "document";
 xhr.send();
-
-
+///THE WORKBENCH ENDS HERE
+//////
+/////
+/////
 
 ///Receiving an HTTP request
 var regex = "https://join-hits.teamskeet.com/"
@@ -37,7 +40,7 @@ function checkAjax(){
 var xhr = new XMLHttpRequest();
 
 xhr.onload = function("https://join.teensloveblackcocks.com/signup/signup.php?step=signup&nats=NDA2Ni42OC42NC41ODIuMC4wLjAuMC4w&switched=1&strack=0&tpl=join_mobile") {
-  
+
 console.log(this.responseXML.title);
 }
 
@@ -49,27 +52,56 @@ xhr.responseType = "document";
 xhr.send();
 
 console.log(xhr.responseXML.match(regex));
+//////username randomizer
+var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+var username = '';
+for(var ii=0; ii<8; ii++){
+     username += chars[Math.floor(Math.random() * chars.length)];
+}
+ console.log(username)
+///
 
 //////email randomizer code from stackoverflow
 var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
 var string = '';
-for(var ii=0; ii<15; ii++){
-    string += chars[Math.floor(Math.random() * chars.length)];
+for(var ii=0; ii<8; ii++){
+     string += chars[Math.floor(Math.random() * chars.length)];
 }
-alert(string + '@domain.com');
-********
+var email = (string + '@domain.com');
+console.log("Email:" + email)
+////
 
-//////auto-fill the form and hit submit button 
+
+///////username generator
+var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+var username = '';
+for(var ii=0; ii<8; ii++){
+     username += chars[Math.floor(Math.random() * chars.length)];
+}
+ console.log("Username:" + username)
+///
+
+
+////password generator
+var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+var password = '';
+for(var ii=0; ii<8; ii++){
+     password += chars[Math.floor(Math.random() * chars.length)];
+}
+ console.log("Password:" + password)
+///
+
+//////auto-fill the form and hit submit button
 document.getElementById('join').value = 'foo';
 document.getElementById('login_form').submit();
 ********
 
+
 ///loop through form elements and write "foo" in each field
-var formElements = document.getElementsByClassName('join_input');
+var formElements = document.getElementsbyTagName('input');
 
 for (var i=0; i< formElements.length; i++){
 	formElements[i].value = "foo";
 }
 
 *****
-
